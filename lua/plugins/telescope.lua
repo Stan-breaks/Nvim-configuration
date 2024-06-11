7fa2d995acd3fad6a34ca34c8cef622d8d07edbf
Stan-breaks <stanleymwendwa03@gmail.com>
2024-06-11 15:20:12 +0300

    telescope default

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,21 +11,6 @@ local telescope_setup = function()
 				},
 			},
 		},
-		pickers = {
-			find_files = {
-				theme = "dropdown",
-				previewer = false,
-				hidden = true,
-			},
-			live_grep = {
-				theme = "dropdown",
-				previewer = false,
-			},
-			find_buffers = {
-				theme = "dropdown",
-				previewer = false,
-			},
-		},
 	})
 end
 
