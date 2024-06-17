1bf105096fbddb7c480cba6d550155c91432aa5a
Stan-breaks <stanleymwendwa03@gmail.com>
2024-06-17 10:09:08 +0300

    edit

--- lua/plugins/copilot-cmp.lua
+++ lua/plugins/copilot-cmp.lua
@@ -1,5 +1,5 @@
 local config = function()
-	require("copilot").setup(require("copilot").setup({
+	require("copilot").setup({
 		panel = {
 			enabled = true,
 			auto_refresh = false,
@@ -41,7 +41,7 @@ local config = function()
 		},
 		copilot_node_command = "node",
 		server_opts_overrides = {},
-	}))
+	})
 	require("copilot_cmp").setup({
 		formatters = {
 			insert_text = require("copilot_cmp.format").remove_existing,
--- lua/plugins/vim-fugitive.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"tpope/vim-fugitive",
-	cmd = "Git",
-	lazy = false,
-}
