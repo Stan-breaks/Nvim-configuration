c5c9d3743fd1a70e677d3932e6e5e9e0615cb078
Stan-breaks <stanleymwendwa03@gmail.com>
2024-07-21 14:09:01 +0300

    new config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,33 +1,59 @@
--- auto-format on save
+-- Auto-format on save
 local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-	group = lsp_fmt_group,
-	callback = function()
-		local efm = vim.lsp.get_clients({ name = "efm" })
+  group = lsp_fmt_group,
+  callback = function(args)
+    -- Get active clients for the current buffer
+    local clients = vim.lsp.get_clients({ bufnr = args.buf })
 
-		if vim.tbl_isempty(efm) then
-			return
-		end
+    if #clients == 0 then return end
 
-		vim.lsp.buf.format({ name = "efm", async = true })
-	end,
+    -- Prioritized list of formatters
+    local preferred_formatters = {
+      "lua_ls",        -- for Lua
+      "tsserver",      -- for JavaScript/TypeScript
+      "gopls",         -- for Go
+      "rust_analyzer", -- for Rust
+      -- Add more language-specific formatters here
+      "efm",           -- as a fallback generic formatter
+      -- nil -- as a last resort, try any available formatter
+    }
+
+    local formatter_to_use = nil
+    for _, formatter in ipairs(preferred_formatters) do
+      for _, client in ipairs(clients) do
+        if client.name == formatter then
+          formatter_to_use = formatter
+          break
+        end
+      end
+      if formatter_to_use then break end
+    end
+
+    vim.lsp.buf.format({
+      async = false,
+      name = formatter_to_use, -- Will be nil if no preferred formatter found
+      timeout_ms = 5000,
+      bufnr = args.buf
+    })
+  end,
 })
---markdown autoformatting
 
+
+--markdown autoformatting
 vim.api.nvim_create_augroup("Markdown", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.opt_local.textwidth = 101
-	end,
-	group = "Markdown",
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.textwidth = 101
+  end,
+  group = "Markdown",
 })
-
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = "*.md",
-	callback = function()
-		vim.api.nvim_command("normal! gg5jVGgq")
-	end,
-	group = "Markdown",
+  pattern = "*.md",
+  callback = function()
+    vim.api.nvim_command("normal! gg5jVGgq")
+  end,
+  group = "Markdown",
 })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,18 +7,18 @@ keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = tr
 keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 --pane and window Navigations
-keymap.set("n", "<C-h>", "C-w>h", opts) -- Navigate Left
-keymap.set("n", "<C-j>", "C-w>j", opts) -- Navigate Down
-keymap.set("n", "<C-k>", "C-w>k", opts) -- Navigate Up
-keymap.set("n", "<C-l>", "C-w>l", opts) -- Navigate Right
+keymap.set("n", "<C-h>", "C-w>h", opts)               -- Navigate Left
+keymap.set("n", "<C-j>", "C-w>j", opts)               -- Navigate Down
+keymap.set("n", "<C-k>", "C-w>k", opts)               -- Navigate Up
+keymap.set("n", "<C-l>", "C-w>l", opts)               -- Navigate Right
 keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts) -- Navigate Left
 keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts) -- Navigate Down
 keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts) -- Navigate Up
 keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts) -- Navigate Right
 
 -- widow Management
-keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split vertically
-keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)          -- Split vertically
+keymap.set("n", "<leader>sh", ":split<CR>", opts)           -- Split Horizontally
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimize
 
 -- indenting
@@ -29,6 +29,5 @@ keymap.set("v", ">", ">gv")
 vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
 
---copilot
-keymap.set("n", "<leader>ce", ":Copilot enable<CR>")
-keymap.set("n", "<leader>cd", ":Copilot disable<CR>")
+--noice
+keymap.set("n", "<leader>ds", ":NoiceDismiss<CR>")
