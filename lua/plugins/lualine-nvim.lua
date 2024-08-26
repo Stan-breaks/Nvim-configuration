e09bbc5475048e5c110c52e9f662dbe7ff3495a6
Stan-breaks <stanleymwendwa03@gmail.com>
2024-08-26 13:49:44 +0300

    lualine diagnosis

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,42 +1,41 @@
 local config = function()
-	local theme = require("lualine.themes.nightfox")
+  local theme = require("lualine.themes.nightfox")
+  --set bg transparency in all modes
+  if theme.normal.c then
+    theme.normal.c.bg = nil
+  end
+  if theme.insert.c then
+    theme.insert.c.bg = nil
+  end
+  if theme.visual.c then
+    theme.visual.c.bg = nil
+  end
+  if theme.replace.c then
+    theme.replace.c.bg = nil
+  end
+  if theme.command.c then
+    theme.command.c.bg = nil
+  end
 
-	--set bg transparency in all modes
-	if theme.normal.c then
-		theme.normal.c.bg = nil
-	end
-	if theme.insert.c then
-		theme.insert.c.bg = nil
-	end
-	if theme.visual.c then
-		theme.visual.c.bg = nil
-	end
-	if theme.replace.c then
-		theme.replace.c.bg = nil
-	end
-	if theme.command.c then
-		theme.command.c.bg = nil
-	end
-
-	require("lualine").setup({
-		options = {
-			theme = theme,
-			globalstatus = true,
-		},
-		tabline = {
-			lualine_a = { "mode" },
-			lualine_b = { "windows" },
-			lualine_c = { "branch" },
-			lualine_x = { "encoding", "fileformat", "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
-		sections = {},
-	})
+  require("lualine").setup({
+    options = {
+      theme = theme,
+      globalstatus = true,
+    },
+    tabline = {
+      lualine_a = { "mode" },
+      lualine_b = { "windows" },
+      lualine_c = { "branch", "diagnostics" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+    sections = {},
+  })
 end
 
 return {
-	"nvim-lualine/lualine.nvim",
-	lazy = false,
-	config = config,
+  "nvim-lualine/lualine.nvim",
+  lazy = false,
+  config = config,
 }
