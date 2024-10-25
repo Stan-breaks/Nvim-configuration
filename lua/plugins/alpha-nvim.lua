79c64a0ec76448255e7430071adbdc2e30c6103a
Stan-breaks <stanleymwendwa03@gmail.com>
2024-10-25 08:37:33 +0300

    alpha update

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -1,9 +1,106 @@
 local config = function()
-	require("alpha").setup(require("alpha.themes.dashboard").config)
+  local alpha = require("alpha")
+  local dashboard = require("alpha.themes.dashboard")
+  dashboard.section.header.val = {
+    [[                                ]],
+    [[             ,,,,,,             ]],
+    [[         o#'9MMHb':'-,o,        ]],
+    [[      .oH":HH$' "' ' -*R&o,     ]],
+    [[     dMMM*""'`'      .oM"HM?.   ]],
+    [[   ,MMM'          "HLbd< ?&H\   ]],
+    [[  .:MH ."\          ` MM  MM&b  ]],
+    [[ . "*H    -        &MMMMMMMMMH: ]],
+    [[ .    dboo        MMMMMMMMMMMM. ]],
+    [[ .   dMMMMMMb      *MMMMMMMMMP. ]],
+    [[ .    MMMMMMMP        *MMMMMP . ]],
+    [[      `#MMMMM           MM6P ,  ]],
+    [[  '    `MMMP"           HM*`,   ]],
+    [[   '    :MM             .- ,    ]],
+    [[    '.   `#?..  .       ..'     ]],
+    [[       -.   .         .-        ]],
+    [[         ''-.oo,oo.-''          ]],
+    [[                                ]],
+  }
+
+  dashboard.section.buttons.val = {
+    dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+    dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+    dashboard.button("g", "  Find text", ":Telescope live_grep<CR>"),
+    dashboard.button("c", "  Config", ":e $MYVIMRC<CR>"),
+    dashboard.button("l", "  Lazy", ":Lazy<CR>"),
+    dashboard.button("q", "  Quit", ":qa<CR>"),
+  }
+  -- Custom footer with Lazy plugin count
+  local function footer()
+    local stats = require("lazy").stats()
+    local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+    return "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+  end
+
+  dashboard.section.footer.val = footer()
+
+  -- Layout configuration
+  dashboard.config.layout = {
+    { type = "padding", val = 2 },
+    dashboard.section.header,
+    { type = "padding", val = 2 },
+    dashboard.section.buttons,
+    { type = "padding", val = 1 },
+    dashboard.section.footer,
+  }
+
+  -- Highlight groups
+  dashboard.section.header.opts.hl = "String"
+  dashboard.section.buttons.opts.hl = "Keyword"
+  dashboard.section.footer.opts.hl = "Comment"
+
+  -- Hide UI elements on dashboard
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "alpha",
+    callback = function()
+      vim.opt.laststatus = 0
+      vim.opt.showtabline = 0
+      vim.opt.cmdheight = 0
+
+      -- Reset when leaving alpha
+      vim.api.nvim_create_autocmd("BufUnload", {
+        buffer = 0,
+        callback = function()
+          vim.opt.laststatus = 3
+          vim.opt.showtabline = 2
+          vim.opt.cmdheight = 1
+        end,
+      })
+    end,
+  })
+
+  alpha.setup(dashboard.config)
+
+  -- Automatically open alpha when no arguments are provided
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      local should_skip = false
+      if vim.fn.argc() > 0 or vim.fn.line2byte(vim.fn.line('$')) ~= -1 or not vim.o.modifiable then
+        should_skip = true
+      else
+        for _, arg in pairs(vim.v.argv) do
+          if arg == '-b' or arg == '-c' or vim.startswith(arg, '+') or arg == '-S' then
+            should_skip = true
+            break
+          end
+        end
+      end
+      if not should_skip then
+        require('alpha').start()
+      end
+    end,
+  })
 end
 
 return {
-	"Stan-breaks/alpha-nvim",
-	event = "BufWinEnter",
-	config = config,
+  "goolord/alpha-nvim",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  event = "BufWinEnter",
+  config = config,
 }
