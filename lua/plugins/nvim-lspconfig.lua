54d206d57d924c2336b4cf9542eb143cc0016a47
Stan-breaks <stanleymwendwa03@gmail.com>
2024-10-29 13:58:29 +0300

    sql lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -178,10 +178,38 @@ local config = function()
     },
   })
   --sql
-  lspconfig.sqls.setup({
+
+  lspconfig.sqlls.setup({
     capabilities = capabilities,
     on_attach = on_attach,
-    filetypes = { "sql" }
+    filetypes = { "sql" },
+    root_dir = lspconfig.util.root_pattern(".git", "*.sql"),
+    settings = {
+      sqlls = {
+        connections = {
+          {
+            name = "PostgreSQL",
+            adapter = "postgresql",
+            driver = 'postgresql',
+            server = "localhost",
+            port = 5432,
+            database = "psql_db",
+            username = "stan",
+            password = "------", -- Consider using environment variables for credentials
+          },
+          {
+            name = "MySQL",
+            adapter = "mysql",
+            driver = 'mysql',
+            server = "127.0.0.1",
+            port = 3306,
+            database = "sql_db",
+            username = "stan",
+            password = "-----", -- Consider using environment variables for credentials
+          }
+        }
+      }
+    }
   })
 
 
@@ -201,8 +229,21 @@ local config = function()
   local golangci_lint = require("efmls-configs.linters.golangci_lint")
   local gofumpt = require("efmls-configs.formatters.gofumpt")
   local google_java_format = require("efmls-configs.formatters.google_java_format")
-  local sqlfmt = require("efmls-configs.formatters.sqlfmt")
-  local sqlfluff = require("efml-configs.linters.sqlfluff")
+
+  -- Configure efm for SQL formatting and linting
+  local sqlfluff = {
+    prefix = "sqlfluff",
+    formatCommand = "sqlfluff fix --dialect postgres -",
+    formatStdin = true,
+    lintCommand = "sqlfluff lint --dialect postgres -",
+    lintStdin = true,
+    lintFormats = { "%f:%l:%c: %m" },
+  }
+
+  local sqlfmt = {
+    formatCommand = "sqlfmt -",
+    formatStdin = true,
+  }
 
   -- configure efm server
   lspconfig.efm.setup({
