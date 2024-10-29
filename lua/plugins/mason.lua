e2aec98fcd3a05d7011c71d37cb0e6b7e780a716
Stan-breaks <stanleymwendwa03@gmail.com>
2024-10-29 10:29:13 +0300

    mason automation

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,71 @@
-return {
-  "williamboman/mason.nvim",
-  cmd = "Mason",
-  event = "BufReadPre",
-  opts = {
+local config = function()
+  require("mason").setup({
     ui = {
       icons = {
         package_installed = "✓",
         package_pending = "➜",
-        package_uninstalled = "✗",
+        package_uninstalled = "✗"
       }
     }
-  }
+  })
+
+  require("mason-lspconfig").setup({
+    -- Automatically install LSP servers
+    automatic_installation = true,
+    ensure_installed = {
+      -- LSP Servers
+      "lua_ls",
+      "rust_analyzer",
+      "jsonls",
+      "pyright",
+      "tsserver",
+      "bashls",
+      "emmet_ls",
+      "dockerls",
+      "clangd",
+      "intelephense",
+      "gopls",
+      "jdtls",
+      "sqlls",
+    },
+  })
+
+  -- Ensure formatters and linters are installed
+  require("mason-null-ls").setup({
+    automatic_installation = true,
+    ensure_installed = {
+      -- Formatters
+      "stylua",
+      "black",
+      "prettier",
+      "fixjson",
+      "shfmt",
+      "clang-format",
+      "rustfmt",
+      "gofumpt",
+      "google-java-format",
+      "sqlfmt",
+
+      -- Linters
+      "luacheck",
+      "flake8",
+      "eslint_d",
+      "shellcheck",
+      "hadolint",
+      "cpplint",
+      "golangci-lint",
+      "sqlfluff",
+    },
+  })
+end
+
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "jay-babu/mason-null-ls.nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  config = config,
 }
