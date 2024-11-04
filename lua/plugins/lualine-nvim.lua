1b47f2a4b0942e41e7478250cd86d8851e948d0c
Stan-breaks <stanleymwendwa03@gmail.com>
2024-11-04 15:04:49 +0300

    lualine bubble

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -22,14 +22,27 @@ local config = function()
       theme = theme,
       globalstatus = true,
       section_separators = { left = '', right = '' },
+      component_separators = "",
     },
     tabline = {
-      lualine_a = { "mode" },
-      lualine_b = { "windows" },
-      lualine_c = { "branch", "diagnostics" },
-      lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
+      lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+      lualine_b = { 'filename', 'branch' },
+      lualine_c = {
+        '%=', --[[ add your center compoentnts here in place of this comment ]]
+      },
+      lualine_x = {},
+      lualine_y = { 'filetype', 'progress' },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 2 },
+      },
+    },
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
     },
     sections = {},
   })
