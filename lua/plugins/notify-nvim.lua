85717d2d217727427286d73bc1ff8f4433e0de01
Stan-breaks <stanleymwendwa03@gmail.com>
2024-11-05 05:54:07 +0300

    shorter notification

--- lua/plugins/notify-nvim.lua
+++ lua/plugins/notify-nvim.lua
@@ -1,7 +1,7 @@
 local config = function()
   require("notify").setup({
     stages = "fade_in_slide_out",
-    timeout = 2000,
+    timeout = 1000,
     background_colour = "#000000",
     icons = {
       ERROR = "",
