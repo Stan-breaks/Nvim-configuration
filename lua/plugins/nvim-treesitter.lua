8a4131987ba568367263f6cd9a80562afe776ca7
Stan-breaks <stanleymwendwa03@gmail.com>
2024-11-17 11:13:45 +0300

    fix treesitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,58 +1,57 @@
 local config = function()
-	require("nvim-treesitter.configs").setup({
-		build = ":TSUpdate",
-		indent = {
-			enable = true,
-		},
+  require("nvim-treesitter.configs").setup({
+    build = ":TSUpdate",
+    indent = {
+      enable = true,
+    },
 
-		event = {
-			"BufReadPre",
-			"BufNewFile",
-		},
-		ensure_installed = {
-			"rust",
-			"markdown",
-			"json",
-			"javascript",
-			"typescript",
-			"yaml",
-			"html",
-			"css",
-			"markdown",
-			"bash",
-			"lua",
-			"dockerfile",
-			"gitignore",
-			"python",
-			"vue",
-			"svelte",
-			"toml",
-			"markdown_inline",
-			"regex",
-			"jsonc",
-			"vimdoc",
-			"vim",
-			"go",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-s>",
-				node_incremental = "<C-s>",
-				scope_incremental = false,
-				node_decremental = "<BS>",
-			},
-		},
-	})
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    ensure_installed = {
+      "rust",
+      "markdown",
+      "json",
+      "javascript",
+      "typescript",
+      "yaml",
+      "html",
+      "css",
+      "bash",       -- Removed duplicate "markdown"
+      "lua",
+      "dockerfile",
+      "gitignore",
+      "python",
+      "vue",
+      "svelte",
+      "toml",
+      "markdown_inline",
+      "regex",
+      "jsonc",
+      "vimdoc",
+      "vim",
+      "go",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-s>",
+        node_incremental = "<C-s>",
+        scope_incremental = false,
+        node_decremental = "<BS>",
+      },
+    },
+  })
 end
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	config = config,
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  config = config,
 }
